46ebf7ba73189522957155d9b8aa1c02461f68cd
edonson2016 <edonson2016@gmail.com>
2024-03-11 14:17:51 -0700

    please

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
 
 function App() {
+
+  async function addEmployee(newEmployee) {
+    try{
+      const response = await fetch("http://localhost:3001/api", {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(newEmployee),
+      });
+      console.log(response);
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+  
+  const submitHandler = (e) => {
+
+    e.preventDefault()
+    const newEmployee = {
+      empId: e.target.empId.value,
+      fname: e.target.fname.value,
+      lname: e.target.lname.value,
+      pri_skill: e.target.pri_skill.value,
+      location: e.target.location.value
+    }
+
+    addEmployee(newEmployee).then(() => {
+      console.log(newEmployee)
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <form onSubmit={submitHandler}>
+        <input type="text" name = "empId" />
+        <input type="text" name = "fname" />
+        <input type="text" name = "lname" />
+        <input type="text" name = "pri_skill" />
+        <input type="text" name = "location" />
+        <input type="submit"/>
+        </form>
+      </div>
     </div>
   );
 }
